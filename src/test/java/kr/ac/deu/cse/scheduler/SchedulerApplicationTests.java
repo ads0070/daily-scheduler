9b9a71c9a38c51cf2777507ebbce5869e9c7de95
Dong-Young Kim <31337.persona@gmail.com>
2021-04-23 21:10:20 +0900

    feat: initial commit

---
+++ src/test/java/kr/ac/deu/cse/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.deu.cse.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
