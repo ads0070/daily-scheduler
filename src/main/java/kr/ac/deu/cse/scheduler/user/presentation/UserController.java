f2846c8feb0b5e0ee51bee31f7e55dd6f5f1677b
Dong-Young Kim <31337.persona@gmail.com>
2021-05-05 18:44:59 +0900

    style: chang annotation order

--- src/main/java/kr/ac/deu/cse/scheduler/user/presentation/UserController.java
+++ src/main/java/kr/ac/deu/cse/scheduler/user/presentation/UserController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@RepositoryRestController
 @RequestMapping("/users")
+@RepositoryRestController
 public class UserController {
 
   private final UserService userService;
