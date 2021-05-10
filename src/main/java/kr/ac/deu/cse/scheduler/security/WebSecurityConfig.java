c86592c092679f20f44dcf827a6bb35331c7bc82
Dong-Young Kim <31337.persona@gmail.com>
2021-05-10 22:03:39 +0900

    refactor(security): rename web security file

--- src/main/java/kr/ac/deu/cse/scheduler/security/WebSecurityConfig.java
+++ src/main/java/kr/ac/deu/cse/scheduler/security/WebSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
